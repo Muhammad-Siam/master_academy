cbf4569292d5dbceefed61e1644e4bb568ea5c5b
Muhammad_Siam <76716941+Muhammad-Siam@users.noreply.github.com>
2021-01-31 21:45:35 +0600

    Add files via upload
    upload

---
+++ variables.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	//var name string = "SIAM"
+       	//var name2 ,  city string = "SIAM" , "DHAKA"
+	//fmt.Println(name2, city, name)
+	
+	/*
+	var name string = "SIAM"
+	var name2 , city string = "Master_Academy" , "DHAKA"
+	fmt.Println(name, city, name2)
+	*/
+	
+	//char
+	var c rune = 'A'
+	var age int = 10
+	var result float32 =50.95
+
+	fmt.Println(c, age, result)
+	
+}
+
