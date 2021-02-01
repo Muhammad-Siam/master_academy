1fe510b2af080d72026ac5dfcbdfc928e8db94aa
Muhammad_Siam <76716941+Muhammad-Siam@users.noreply.github.com>
2021-02-01 12:40:15 +0600

    I made a change

--- variables.go
+++ variables.go
@@ -17,9 +17,16 @@ func main(){
 	//char
 	var c rune = 'A'
 	var age int = 10
-	var result float32 =50.95
+	var result3 float32 =50.95
 
-	fmt.Println(c, age, result)
+	fmt.Println(c, age, result3)
+
+	var name string = "Siam"
+	var subject , subject2 string = "Physics" , "Chemestry"
+	var result, result1 float32 =97.0 , 98.02
+
+	fmt.Println(name, subject, subject2, result, result1)
+	
 	
 }
 
