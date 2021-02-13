5a9e00c15d09a24c65b1fc31bffa2c683e079fb8
Muhammad_Siam <[rootersiam2255@gamil.com]>
2021-02-13 13:24:36 +0600

    Struct datatype

---
+++ custom.go
@@ -0,0 +1,70 @@
+package main
+
+type Weight float32
+
+var name string = "SIAM"
+
+//array
+//slice
+//map
+//struct= অনেক গুল টাইপ কে একত্র করে.
+//struct: user defined data types, composited data types, concrete data types
+//struct e onek gulo promitive data type use kora hoy ar struct sobceye besi bebohar hoy
+
+//syntax:
+
+/*type Book struct{
+ Title string
+ Author string
+ ISBN string
+ Price float32
+ Pages int
+
+}
+
+var b1 Book
+b1.Title="An Introduction to pragramming in Go"
+b1.Author = "CALEB DOXY"
+b1.ISBN = "978-1478355823"
+b1.Price = 10.50
+b1.Pages = 165
+fmt.Println(b1)
+fmt.Println(b1.Title)
+fmt.Println(b1.Author)
+fmt.Println(b1.ISBN)
+fmt.Println(b1.Price)
+fmt.Println(b1.Pages)
+
+type Student struct{
+Color string
+Name string
+Age float32
+Roll int
+}
+
+var a1 Student
+a1.Color = "White"
+a1.Name = "Siam"
+a1.Age = 16.5
+a1.Roll = 1
+ 
+fmt.Println(a1)
+fmt.Println(a1.Color)
+fmt.Println(a1.Name)
+fmt.Println(a1.Age)
+fmt.Println(a1.Roll)
+//Struct literals
+b1 := struct{
+  Title string
+  Author string
+  ISBN string
+  Price float32
+  Pages int
+}{
+Title: "An introduction to programming in Go",
+Author: "CALEB DOXY",
+ISBN: "978-1478832",
+Price: 10.50,
+Pages: 165,
+}
+fmt.Println(b1)*/
